467194ce695652263ccf7365b716b581beb131f1
Rajveer <rajveer125112@gmail.com>
2026-06-27 19:40:37 +0530

    Day 6 done

--- 06-Day6-Variables-and-Data-Types/main.py
+++ 06-Day6-Variables-and-Data-Types/main.py
@@ -1,21 +1,34 @@
-a = complex(8, 2)
-b = True
-c = "Harry"
-d = None
-print(a)
-print(b)
-a1 = 9
-print(a + a1)
-print("The type of a is ", type(a))
-print("The type of b is ", type(b))
-print("The type of c is ", type(c))
+#Learning Types of datatype and storing them into a variable
 
+a=12  #integer
+b=12.3 #float/decimal
+c=complex(23) #complex
+d="Nice" #string
+e=True #boolean
+f=None #null
 
-list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+#Learning how to run commang type() to find type of data stored in a variable
+
+print("The type of a is ",type(a))
+print("The type of b is ",type(b))
+print("The type of c is ",type(c))
+print("The type of d is ",type(d))
+print("The type of e is ",type(e))
+print("The type of f is ",type(f))
+
+#Other Datatypes I learned
+
+#SEQUENCED DATA 
+
+list1=[1,2,3,4,["Nice day"],True,False] #list data type used to store ordered data it is mutable and multiple type of data can be stored in it.
 print(list1)
 
-tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
-print(tuple1)
+tuple1=("Nice","Day","Night",2,34,31)#tuple data type is used to store ordered data and it is immutable and multiple data types can be store in it.
+print(tuple1) 
 
-dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+#Mapped data
+
+dict1={"Name":"Rajveer","Age":18}#dictionary is unordered collection of datatype which is used to assign key value to each data stored.
 print(dict1)
+
+
