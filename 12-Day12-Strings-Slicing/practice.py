d6184152989f71cc1fb6b6332741c5d5e457c0cf
Rajveer <rajveer125112@gmail.com>
2026-06-29 19:42:59 +0530

    Day 12 done

---
+++ 12-Day12-Strings-Slicing/practice.py
@@ -0,0 +1,16 @@
+#Using len() function to find length of string
+
+name="Rajveer"
+print(len(name))
+
+#String Slicing
+
+print(name[:5])
+print(name[0:3])
+print(name[-3:-1])
+print(name[-2:])
+
+#Looping
+
+for i in name:
+    print(i)
