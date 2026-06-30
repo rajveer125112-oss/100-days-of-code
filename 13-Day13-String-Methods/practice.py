07d73298aa122b5903819bc7a98e6abd367c2c5c
Rajveer <rajveer125112@gmail.com>
2026-06-30 20:46:00 +0530

    Day 13 done

---
+++ 13-Day13-String-Methods/practice.py
@@ -0,0 +1,25 @@
+name="We are Good !!!!!!!!!!"
+
+print("1",name.upper())              #All letters are converted to upper case
+print("2",name.lower())              #All letters are converted to lower case
+print("3",name.strip())              #Strips away Gaps present in the string
+print("4",name.rstrip("!"))          #Strips away only specified text in string
+print("5",name.split(" "))           #Splits the string into list
+print("6",name.capitalize())         #Capitalizes first letter of the string
+print("7",name.center(64,"`"))       #Centers the string with defined value
+print("8",name.count("o"))           #Counts number of specified letter present in the string
+print("9",name.endswith("99"))       #Checks whether string ends with the specified text
+print("10",name.find("a"))           #Finds the specified text in the string
+print("11",name.index("Good"))       #Finding location where the specified text exists first
+print("12",name.isalnum())           #Checks whether the string is alphanumeric or not
+print("13",name.isalpha())           #Checks whether the string is alphabetical or not
+print("14",name.islower())           #Checks whether the string is lowercased or not
+print("15",name.isnumeric())         #Checks whether the string is numeric or not
+print("16",name.isprintable())       #Checks whether the string is printable or not
+print("17",name.istitle())           #Checks whether the string is a title or not
+print("18",name.isupper())           #Checks whether the string is uppercased or not
+print("19",name.isspace())           #Checks whether the string has only white spaces
+print("20",name.swapcase())          #Swaps lower case with upper case and vice versa
+print("21",name.title())             #Converts string into title
+
+
