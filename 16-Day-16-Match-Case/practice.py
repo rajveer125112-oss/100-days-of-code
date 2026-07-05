f3ef3f311744526abcf0a65de2024334a8a6cb98
Rajveer <rajveer125112@gmail.com>
2026-07-05 21:51:22 +0530

    Day 16 and Day 17 done

--- 16-Day-16-Match-Case/practice.py
+++ 16-Day-16-Match-Case/practice.py
@@ -7,3 +7,5 @@ match x:
         print("The number is odd")
     case 87:
         print("the number is 87")
+
+#if earlier case is satisfied then next case won't be satisfied case _ is defined as default or empty case and case (some no.) means it refers to case where x=(some number),we can also use if commands as displayed
---
+++ 17-Day17-For-Loops/practice.py
@@ -0,0 +1,27 @@
+#For a table of 2:
+
+for i in range(1,21):
+    print(2*i)
+
+list1=["Red","Orange","Blue"]
+
+
+
+a="rajveer"
+
+for i in a:
+    print(i)
+    if i=="r":
+        print("nice")
+
+
+list1=["Red","Orange","Blue"]
+
+for i in list1:
+    if i=="Orange":
+        print("I hate Orange")
+    print(i)
+
+for i in range(1,16,2):
+    print(i)
+
