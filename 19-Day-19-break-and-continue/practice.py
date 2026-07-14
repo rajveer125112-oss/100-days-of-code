07e371c6f4602d6cd45e28ac9ad471401529c7df
Rajveer <rajveer125112@gmail.com>
2026-07-14 23:24:50 +0530

    Day 19 done

---
+++ 19-Day-19-break-and-continue/practice.py
@@ -0,0 +1,20 @@
+for i in range(1,101,1):
+    print(i ,end=" ")
+    if(i==50):
+        break
+    else:
+        print("Mississippi")
+print("Thank you")
+
+
+for i in [2,3,4,6,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
+
+for j in range(1,100):
+    if j==34:
+        break
+    else:
+        print("yes")
+print("meow")
