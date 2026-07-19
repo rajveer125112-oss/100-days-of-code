fceef3f945e035794154342461f35aedbbf8255b
Rajveer <rajveer125112@gmail.com>
2026-07-19 19:50:59 +0530

    Tried Making KBC project

--- 27-Day27-Exercise-3/practice.py
+++ 27-Day27-Exercise-3/practice.py
@@ -1,4 +1,12 @@
-
+def ans(i,j):
+    n=0
+    if j==answers[i]:
+            print("Answer is correct")
+            n=n+1000*(i+1)
+            print("You won",n,"rupees")
+    else:
+          print("Answer is incorrect")
+        
 
 
 
@@ -14,10 +22,9 @@ print(q1[2])
 print(q1[3])
 print(q1[4])
 
-
-
-
-
+ans(0,input("Enter the answer ="))
+      
+     
 
 q2 = [
     'Which state is known as the "Spice Garden of India"?',
@@ -27,11 +34,26 @@ q2 = [
     "D. Tamil Nadu",
 ]
 
-print("The first question is :",q2[0])
+print("The second question is :",q2[0])
 print(q2[1])
 print(q2[2])
 print(q2[3])
 print(q2[4])
 
-print(ans(input("Enter ans =")))
+ans(1,input("Enter the answer ="))
+n=0
+while n<2:
+    m=0
+    if ans(n,input("Enter the answer ="))=="Answer is correct":
+          m=m+1000
+          print("You won total of",m,"rupees")
+    n=n+1
+
+
+
+
+
+
+     
+
 
