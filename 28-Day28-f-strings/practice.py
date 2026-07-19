6bcc7fe667c3e178abf7c1ced973786a613d134c
Rajveer <rajveer125112@gmail.com>
2026-07-19 22:40:23 +0530

    Day 28 complete

---
+++ 28-Day28-f-strings/practice.py
@@ -0,0 +1,7 @@
+name=input("Enter the name")
+country=input("Enter the country")
+
+print(f"My name is {name} I am from {country}")
+print(f"My name is {{name}} I am from {{country}}")
+print(f"{80*6}")
+
