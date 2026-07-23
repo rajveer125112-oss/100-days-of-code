021c819a0766a3ed343315ff1d34de5279179fab
Rajveer <rajveer125112@gmail.com>
2026-07-23 17:21:24 +0530

    Day 35 done

---
+++ 35-Day-35-For-loop-with-else/practice.py
@@ -0,0 +1,20 @@
+for i in range(12):
+    print(i)
+
+else:
+    print("No i")
+
+for j in range(12):
+    if j==2:
+        print(j)
+
+else:
+    print("no 2")
+
+for h in range(12): #break statement does not continue with else
+    if h==11:
+        print(h)
+        break
+else:
+    print("no h")
+
