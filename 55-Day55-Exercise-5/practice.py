e5b14a43b97ca1e939a8e70255cfc664d45bc85c
Rajveer <rajveer125112@gmail.com>
2026-08-02 22:03:18 +0530

    Snake/Water/Gun done

---
+++ 55-Day55-Exercise-5/practice.py
@@ -0,0 +1,24 @@
+import secrets
+
+a=input("Enter your move (snake/water/gun) =")
+comp=["snake","water","gun"]
+
+computer=secrets.choice(comp)
+
+
+
+
+if computer==a:
+    print("draw")
+elif computer=="snake" and a=="water":
+    print("you lost")
+elif computer=="snake" and a=="gun":
+    print("you win")
+elif computer=="water" and a=="snake":
+    print("you win")
+elif computer=="water" and a=="gun":
+    print("you lost")
+elif computer=="gun" and a=="snake":
+    print("you lost")
+elif computer=="gun" and a=="water":
+    print("you win")
